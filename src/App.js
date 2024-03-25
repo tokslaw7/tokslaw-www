72a125e0235f34c78e0f94048d56c9afa9c5170b
tokslaw-shruboak <TLawal@shruboak.org>
2024-03-25 10:09:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import logo from './Tokslaw.png';
-import './App.css';
+// import logo from './Tokslaw.png';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>Coming Soon!</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Head from "next/head";
+import { Fragment } from "react";
+import ColorState from "../src/context/colorState";
+import NavState from "../src/context/navState";
+import PreLoader from "../src/layouts/PreLoader";
+import './App.css';
+//import logo from './Tokslaw.png';
+import "../styles/globals.css";
+
+function App({ Component, pageProps }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coming Soon!</h1>
-      </header>
-    </div>
-  );
+    <Fragment>
+    <Head>
+      
+      <title> Personal Portfolio</title>
+    </Head>
+    <NavState>
+      <ColorState>
+        <Component {...pageProps} />
+        <PreLoader />
+      </ColorState>
+    </NavState>
+  </Fragment>
+);
 }
 
 export default App;
